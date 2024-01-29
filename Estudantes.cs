870b64167cb464595e8c4cfcd2ea4c28913ad1ec
Samuel Barros <samuel.barrosluna@outlook.com>
2024-01-28 22:15:10 -0300

    Primeiro commit C#

---
+++ Estudantes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioQuartos
+{
+    internal class Estudantes
+    {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudantes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Email}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExercicioQuartos // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudantes[] totalQuartos = new Estudantes[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int quartos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quartos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i + 1}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                totalQuartos[quarto] = new Estudantes(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+
+           for (int i = 0;i < totalQuartos.Length;i++)
+            {
+                if (totalQuartos[i] != null)
+                {
+                    Console.WriteLine($"{i} {totalQuartos[i]}");
+                }
+            }
+
+
+        }
+    }
+}
